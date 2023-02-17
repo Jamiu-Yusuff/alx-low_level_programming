45c849528db7e1469574d56286504062e5b51b44
Jamiu-Yusuff <yusuffjamiu@rocketmail.com>
2023-02-17 15:13:08 -0800

    posneg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,27 +3,30 @@
 #include <stdio.h>
 /* more headers goes there */
 /**
- * main - A program that outputs control flows
- * Return: 0 (Success)
+ * main - A program that outputs if
+ * the integer greater than 5 and
+ * negative if integer less than 5
+ * Return: Always 0 (Success)
  */
 /* betty style doc for function main goes there */
+
 int main(void)
 {
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-else if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-else if (n < 0)
-{
-printf("%d is negative\n", n);
-}
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }
