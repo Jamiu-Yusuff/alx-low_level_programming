81009fa904eb34efb845f1250903af657005ee6d
Jamiu-Yusuff <yusuffjamiu@rocketmail.com>
2023-02-17 16:42:50 -0800

    number onetoten)

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints from 0 to 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 0; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
