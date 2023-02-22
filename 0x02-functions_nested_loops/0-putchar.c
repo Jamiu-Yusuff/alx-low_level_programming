ecca11df3f8d81794dd70c7bbbc7edd84af05fbc
Jamiu-Yusuff <yusuffjamiu@rocketmail.com>
2023-02-22 09:47:27 -0800

    Putcharrr

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,18 @@
 #include "main.h"
+
+/**
+ * main - A progam to print
+ * _putchar
+ * REturn: Always 0 (Success)
+ */
+
 int main(void)
 {
 	char ins[] = "_putchar";
 	int i = 0;
 	while (ins[i] != '\0')
 	{
-		_putchar(ins[i]); i++;	
+		_putchar(ins[i]); i++;
 	}
 	_putchar('\n');
 
