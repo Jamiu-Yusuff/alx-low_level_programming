e88ec09063bf483d85fe30ddfaa8944cae58752e
Jamiu-Yusuff <yusuffjamiu@rocketmail.com>
2023-02-24 14:58:42 -0800

    Sizes

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_square - prints square
+ * with newline
+ * Return: ALways 0 (Success)
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+
+			}
+		_putchar('\n');
+		}
+
+	}
+
+
+}
