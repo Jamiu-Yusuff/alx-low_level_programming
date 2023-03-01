d75fde29aa25eaa1cb0e2a5abd87ee86c725ecd5
Jamiu-Yusuff <yusuffjamiu@rocketmail.com>
2023-02-28 17:02:50 -0800

    Resetto 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that takes a pointer to an
+ * int as parameter
+ * updates the value to 98
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
