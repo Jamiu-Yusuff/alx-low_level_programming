fcccf98413a05e03ab5d8f20bc8bc551ffc481c8
Jamiu-Yusuff <yusuffjamiu@rocketmail.com>
2023-02-28 17:14:00 -0800

    SwapPointer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps integers
+ * Return: Always 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+	int m;
+	int a;
+	int b;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}
