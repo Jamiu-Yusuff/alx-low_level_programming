f71a1e9f7db4368fab733e166e89984375705db3
Jamiu-Yusuff <yusuffjamiu@rocketmail.com>
2023-03-02 17:31:57 -0800

    printnum

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*print_number - print numbers
+*characters
+*pointers to string params
+*Return: Always 0 (Success)
+*/
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
+	{
+		_putchar('_');
+		n1 = -n;
+	}
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+}
