005b421d096bb869625c62c9ba62f266e76d3568
Jamiu-Yusuff <yusuffjamiu@rocketmail.com>
2023-03-02 16:33:23 -0800

    StringCat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*_strncat - two using at most n bytes
+*of strings concatenated
+*Return: Always 0 (Success)
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		j++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
